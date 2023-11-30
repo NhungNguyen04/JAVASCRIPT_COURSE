49914ffe267f8429ce5ef774d7501cfe8f746bb1
NGUYEN THI HONG NHUNG <143203553+NhungNguyen04@users.noreply.github.com>
2023-11-30 23:31:31 +0700

    pig game switch player

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,33 +1,42 @@
 'use strict';
 
 // Selecting elements
-const current0 = document.getElementById('#current--0');
-const current1 = document.getElementById('#current--1');
-const total0 = document.getElementById('#score--0');
-const total1 = document.getElementById('#score--1');
+const current0 = document.getElementById('current--0');
+const current1 = document.getElementById('current--1');
+const total0 = document.getElementById('score--0');
+const total1 = document.getElementById('score--1');
 const dice = document.querySelector(".dice");
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-c
+
 // Starting conditions
-current0.textContent = 0
-current1.textContent = 1
+current0.textContent = 0;
+current1.textContent = 0;
+total0.textContent = 0;
+total1.textContent = 0;
 dice.classList.add('hidden');
 let currentScore = 0;
+let activePlayer = 0;
+
 // Rolling dice functionality
 btnRoll.addEventListener('click', function(){
 // 1. Generating a random dice roll
-    const diceRoll = Math.truncate(Math.random()*6)+1;
+    const diceRoll = Math.trunc(Math.random()*6)+1;
+    console.log(diceRoll);
 // 2. Display dice
     dice.classList.remove('hidden');
-    dice.scr = `dice-${diceRoll}.png`
+    dice.src = `dice-${diceRoll}.png`;
 // 3. Check for rolled 1: if true, switch to the next player
-    if (diceRoll != 1) {
+    if (diceRoll !== 1) {
         currentScore += diceRoll;
-        current0.textContent = currentScore;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     }
     else {
-        
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+        activePlayer = activePlayer === 0?1:0;
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--active');
+        currentScore = 0;
     }
 });
