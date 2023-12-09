aad422c2e820ed8a416f7d8ef062b2affff4401e
NGUYEN THI HONG NHUNG <143203553+NhungNguyen04@users.noreply.github.com>
2023-12-09 15:52:26 +0700

    Scoping chain in practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+function calcAge(birthYear) {
+    const age = 2037 -birthYear;
+    console.log(firstName);
+    function printAge() {
+        const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+        console.log(output);
+    }
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+        var millenial = true;
+        const str = `Oh, and your're a millenial, ${firstName}`;
+        console.log(str);
+
+        function add (a, b) {
+            return a + b;
+        }
+    }
+    // console.log(millenial); undefined
+    console.log(add(3, 7));
+}
+
+const firstName = 'Nhung';
+calcAge(2004);
